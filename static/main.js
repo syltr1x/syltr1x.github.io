54e1c320b70bd1338c75d597cd382d5913c60da9
syltr1x <syltr1x@proton.me>
2024-05-17 04:04:42 -0300

    add: hook_item function

--- static/main.js
+++ static/main.js
@@ -1,7 +1,15 @@
 function show_info() {
     console.log("Mostrando")
 }
-function download(url){
+function download(url) {
     window.open(url, "_blank")
+}
+function hook_item(id) {
+    let hooked_item= document.getElementById(id);
+    hooked_item.style.animation = 'remark 0.8s linear'
+    setTimeout(() => {
+    hooked_item.style.animation = ''
+    }, 900)
+    window.location.href = `#${id}`
 }
 show_info()
