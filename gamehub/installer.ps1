66045c2c21c74c8607037d3920a2bb38dec44339
syltr1x <syltr1x@proton.me>
2024-10-02 16:34:43 -0300

    change: move gamehub project to https://github.com/syltr1x/mythings

--- gamehub/installer.ps1
+++
@@ -1,6 +0,0 @@
-mkdir gamehub
-cd gamehub
-mkdir templates
-curl.exe https://syltr1x.github.io/gamehub/app.py -o app.py
-curl.exe https://syltr1x.github.io/gamehub/scrap.py -o scrap.py
-curl.exe https://syltr1x.github.io/gamehub/templates/index.html -o templates\index.html
--- gamehub/installer.sh
+++
@@ -1,6 +0,0 @@
-mkdir gamehub
-cd gamehub
-mkdir templates
-curl https://syltr1x.github.io/gamehub/app.py -o app.py
-curl https://syltr1x.github.io/gamehub/scrap.py -o scrap.py
-curl https://syltr1x.github.io/gamehub/templates/index.html -o templates/index.html
